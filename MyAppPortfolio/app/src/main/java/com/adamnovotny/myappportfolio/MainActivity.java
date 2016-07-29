517a4969de1d75772880b3022e16acdc711cb1d5
Adam Novotny <1@adamnovotny.com>
2016-07-28 22:22:12 -0400

    Optimize toasts in main activity

--- MyAppPortfolio/app/src/main/java/com/adamnovotny/myappportfolio/MainActivity.java
+++ MyAppPortfolio/app/src/main/java/com/adamnovotny/myappportfolio/MainActivity.java
@@ -3,6 +3,7 @@ package com.adamnovotny.myappportfolio;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,45 +16,12 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void launchAppOnclick(View v) {
-        switch(v.getId()) {
-            case R.id.button_popular_movies:
-                Toast toast1 = Toast.makeText(getApplicationContext(),
-                        "This button will launch my Popular Movies App",
-                        Toast.LENGTH_SHORT);
-                toast1.show();
-                break;
-            case R.id.button_stock_hawk:
-                Toast toast2 = Toast.makeText(getApplicationContext(),
-                        "This button will launch my Stock Hawk App",
-                        Toast.LENGTH_SHORT);
-                toast2.show();
-                break;
-            case R.id.button_build_it_bigger:
-                Toast toast3 = Toast.makeText(getApplicationContext(),
-                        "This button will launch my Build It Bigger App",
-                        Toast.LENGTH_SHORT);
-                toast3.show();
-                break;
-            case R.id.button_make_your_app_material:
-                Toast toast4 = Toast.makeText(getApplicationContext(),
-                        "This button will launch my Make Your App Material App",
-                        Toast.LENGTH_SHORT);
-                toast4.show();
-                break;
-            case R.id.button_go_ubiquitous:
-                Toast toast5 = Toast.makeText(getApplicationContext(),
-                        "This button will launch my Go Ubiquitous App",
-                        Toast.LENGTH_SHORT);
-                toast5.show();
-                break;
-            case R.id.button_capstone:
-                Toast toast6 = Toast.makeText(getApplicationContext(),
-                        "This button will launch my Capstone App",
-                        Toast.LENGTH_SHORT);
-                toast6.show();
-                break;
-            default:
-                throw new RuntimeException("Unknow button ID");
-        }
+        Button btn = (Button) v;
+        String btnText = (String) btn.getText();
+        CharSequence txt = getString(R.string.open_app_text) + " " + btnText;
+        Toast toast = Toast.makeText(getApplicationContext(),
+                txt,
+                Toast.LENGTH_SHORT);
+        toast.show();
     }
 }
