92660df7d383a2a4481027742af7d5c2d99eed36
Adam Novotny <1@adamnovotny.com>
2016-08-06 21:21:14 -0400

    feat: moviedb json fetch

--- PopularMovies/app/src/main/java/com/adamnovotny/popularmovies/MovieListFragment.java
+++ PopularMovies/app/src/main/java/com/adamnovotny/popularmovies/MovieListFragment.java
@@ -2,6 +2,8 @@ package com.adamnovotny.popularmovies;
 
 
 import android.content.Context;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -13,18 +15,43 @@ import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class MovieListFragment extends Fragment {
     GridView moviesGrid;
-    // TODO populate movies from API
+    MoviesAdapter moviesAdapter;
     int movies[] = {R.drawable.img1, R.drawable.img1, R.drawable.img1,
-            R.drawable.img1, R.drawable.img1};
+            R.drawable.img1, R.drawable.img1}; ///////////////////////////// delete after test
+    String testImg;
+    ArrayList<MovieParcelable> moviesP = new ArrayList<>();
 
     public MovieListFragment() {
         // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        if(savedInstanceState == null || !savedInstanceState.containsKey("movies")) {
+            updateMovies();
+        }
+        else {
+            moviesP = savedInstanceState.getParcelableArrayList("movies");
+        }
     }
 
     @Override
@@ -33,15 +60,33 @@ public class MovieListFragment extends Fragment {
         // Inflate the layout for this fragment, populate content, set onclick
         View view = inflater.inflate(R.layout.fragment_movie_list, container, false);
         moviesGrid = (GridView) view.findViewById(R.id.moviesGridView);
-        MoviesAdapter moviesAdapter = new MoviesAdapter(getActivity(), movies);
+        moviesAdapter = new MoviesAdapter(getActivity(), movies);
         moviesGrid.setAdapter(moviesAdapter);
         moviesGrid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Log.i("MovieListFragment", "Clicked: " + position);
+                Log.i("MovieListFragment", "Pressed: " + position); /////////////////////////
             }
         });
         return view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        updateMovies();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        outState.putParcelableArrayList("movies", moviesP);
+        super.onSaveInstanceState(outState);
+    }
+
+    private void updateMovies() {
+        GetMovieData movieDataTask = new GetMovieData();
+        String[] sortType = {"popularity.desc", "vote_average.desc"};
+        movieDataTask.execute(sortType[0]);
     }
 
     class MoviesAdapter extends BaseAdapter {
@@ -74,8 +119,103 @@ public class MovieListFragment extends Fragment {
                 view = inflater.inflate(R.layout.fragment_movie_list_item, null);
             }
             ImageView img = (ImageView) view.findViewById(R.id.fragment_movie_list_item_imageview);
-            img.setImageResource(movies[i]);
+            //img.setImageResource(movies[i]);
             return view;
+        }
+    }
+
+    private class GetMovieData extends AsyncTask<String, Void, ArrayList<MovieParcelable>> {
+        private final String LOG_TAG = GetMovieData.class.getSimpleName();
+
+        @Override
+        protected ArrayList<MovieParcelable> doInBackground(String... params) {
+            if (params.length == 0) { // error
+                return null;
+            }
+
+            // Connection objects declared outside try block to be closed in finally
+            HttpURLConnection urlConn = null;
+            BufferedReader reader = null;
+            String urlConnResult;
+            try {
+                final String BASE_URL = "http://api.themoviedb.org/3/discover/movie?";
+                final String PARAM_SORT = "sort_by";
+                final String PARAM_KEY = "api_key";
+                Uri builtUri = Uri.parse(BASE_URL).buildUpon()
+                        .appendQueryParameter(PARAM_SORT, params[0])
+                        .appendQueryParameter(PARAM_KEY, AppKeys.themoviedb)
+                        .build();
+                URL url = new URL(builtUri.toString());
+                urlConn = (HttpURLConnection) url.openConnection();
+                urlConn.setRequestMethod("GET");
+                urlConn.connect();
+                InputStream inputStream = urlConn.getInputStream();
+                // read input
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line + "\n");
+                }
+                if (buffer.length() == 0) {
+                    return null;
+                }
+                urlConnResult = buffer.toString();
+            }
+            catch (IOException e) {
+                Log.e(LOG_TAG, "Error ", e);
+                return null;
+            }
+            finally {
+                if (urlConn != null) {
+                    urlConn.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e(LOG_TAG, "Error closing stream", e);
+                    }
+                }
+            }
+            return updateMoviesFromJson(urlConnResult);
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<MovieParcelable> movies) {
+            moviesP = movies;
+            // TODO update adapter
+            Log.i(LOG_TAG, "Json received");
+            for(int i = 0; i < moviesP.size(); i++) {
+                Log.i("Movie " + i, moviesP.get(i).toString());
+                testImg = moviesP.get(i).image;
+            }
+
+        }
+
+        private ArrayList<MovieParcelable> updateMoviesFromJson(String jsonStr) {
+            ArrayList<MovieParcelable> movies = new ArrayList<>();
+            try {
+                JSONObject moviesJson = new JSONObject(jsonStr);
+                JSONArray moviesArray = moviesJson.getJSONArray("results");
+                for(int i = 0; i < moviesArray.length(); i++) {
+                    JSONObject movieObj = moviesArray.getJSONObject(i);
+                    MovieParcelable movie = new MovieParcelable(
+                            movieObj.getString("title"),
+                            movieObj.getString("poster_path"),
+                            movieObj.getString("overview"),
+                            movieObj.getString("vote_average"),
+                            movieObj.getString("release_date"));
+                    movies.add(movie);
+                }
+            }
+            catch (JSONException e) {
+                Log.e(LOG_TAG, "JSON exception: ", e);
+            }
+            return movies;
         }
     }
 }
---
+++ PopularMovies/app/src/main/java/com/adamnovotny/popularmovies/MovieParcelable.java
@@ -0,0 +1,61 @@
+package com.adamnovotny.popularmovies;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class MovieParcelable implements Parcelable {
+    String title;
+    String image;
+    String overview;
+    String vote;
+    String release;
+
+    public MovieParcelable(String pTitle, String pImage, String pOverview,
+                           String pVote, String pRelease) {
+        title = pTitle;
+        image = pImage;
+        overview = pOverview;
+        vote = pVote;
+        release = pRelease;
+    }
+
+    private MovieParcelable(Parcel in) {
+        title = in.readString();
+        image = in.readString();
+        overview = in.readString();
+        vote = in.readString();
+        release = in.readString();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public String toString() {
+        return title + ". Overview: " + overview + " Vote: " +
+                vote + ". Release: " + release;
+    }
+
+    @Override
+    public void writeToParcel(Parcel p, int i) {
+        p.writeString(title);
+        p.writeString(image);
+        p.writeString(overview);
+        p.writeString(vote);
+        p.writeString(release);
+    }
+
+    public final Parcelable.Creator<MovieParcelable> CREATOR =
+            new Parcelable.Creator<MovieParcelable>() {
+        @Override
+        public MovieParcelable createFromParcel(Parcel parcel) {
+            return new MovieParcelable(parcel);
+        }
+
+        @Override
+        public MovieParcelable[] newArray(int i) {
+            return new MovieParcelable[i];
+        }
+    };
+}
