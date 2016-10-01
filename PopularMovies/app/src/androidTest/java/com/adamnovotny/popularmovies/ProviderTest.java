27e756af5fb512d9bd0b172a7eff7295736af20f
Adam Novotny <1@adamnovotny.com>
2016-10-01 16:28:15 +0200

    feat: add all ContentProvider tests

--- PopularMovies/app/src/androidTest/java/com/adamnovotny/popularmovies/ProviderTest.java
+++ PopularMovies/app/src/androidTest/java/com/adamnovotny/popularmovies/ProviderTest.java
@@ -1,12 +1,13 @@
 package com.adamnovotny.popularmovies;
 
 import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
+import android.net.Uri;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.ActivityInstrumentationTestCase2;
-import android.util.Log;
 
 import com.adamnovotny.popularmovies.data.MovieContract;
 
@@ -41,17 +42,65 @@ public class ProviderTest
     }
 
     @Test
+    public void testInsertProvider() {
+        // insert
+        ContentResolver resolver = mContext.getContentResolver();
+        ContentValues values = new ContentValues();
+        String testId = "123";
+        values.put(MovieContract.MovieEntry.COLUMN_MOVIE_ID, testId);
+        Uri uri = resolver.insert(MovieContract.MovieEntry.CONTENT_URI, values);
+        uri = Uri.parse(uri.toString());
+        String protocol = uri.getScheme();
+        String auth = uri.getAuthority();
+        assertEquals("content", protocol);
+        assertEquals(auth, MovieContract.CONTENT_AUTHORITY);
+        // delete
+        String delStr = MovieContract.MovieEntry.COLUMN_MOVIE_ID + "=" + testId;
+        int rows = resolver.delete(MovieContract.MovieEntry.CONTENT_URI, delStr, null);
+        assertTrue(rows >= 1);
+    }
+
+    @Test
     public void testQueryProvider() {
         ContentResolver resolver = mContext.getContentResolver();
+        // insert
+        String testId = "123";
+        ContentValues values = new ContentValues();
+        values.put(MovieContract.MovieEntry.COLUMN_MOVIE_ID, testId);
+        Uri uri = resolver.insert(MovieContract.MovieEntry.CONTENT_URI, values);
+        // query
         Cursor cursor = resolver.query(MovieContract.MovieEntry.CONTENT_URI,
                 null, null, null, null);
         int count = cursor.getCount();
         assertNotSame(count, 0);
         int favoriteIdCol = cursor.getColumnIndex(MovieContract.MovieEntry.COLUMN_MOVIE_ID);
         cursor.moveToFirst();
-        while(cursor.moveToNext()) {
-            Log.i(LOG_TAG, cursor.getString(favoriteIdCol));
-        }
+        assertEquals(testId, cursor.getString(favoriteIdCol));
+        // delete
+        String delStr = MovieContract.MovieEntry.COLUMN_MOVIE_ID + "=" + testId;
+        int rows = resolver.delete(MovieContract.MovieEntry.CONTENT_URI, delStr, null);
+        assertTrue(rows >= 1);
     }
 
+    @Test
+    public void testUpdateProvider() {
+        // insert
+        ContentResolver resolver = mContext.getContentResolver();
+        ContentValues values = new ContentValues();
+        String testId = "123";
+        values.put(MovieContract.MovieEntry.COLUMN_MOVIE_ID, testId);
+        Uri uri = resolver.insert(MovieContract.MovieEntry.CONTENT_URI, values);
+        // modify
+        String testId2 = "345";
+        values = new ContentValues();
+        values.put(MovieContract.MovieEntry.COLUMN_MOVIE_ID, testId2);
+        String selection = MovieContract.MovieEntry.COLUMN_MOVIE_ID + "=" + testId;
+        int rows = resolver.update(
+                MovieContract.MovieEntry.CONTENT_URI, values, selection, null);
+        assertTrue(rows >= 1);
+        // delete
+        String delStr = MovieContract.MovieEntry.COLUMN_MOVIE_ID + "=" + testId2;
+        rows = resolver.delete(MovieContract.MovieEntry.CONTENT_URI, delStr, null);
+        assertTrue(rows >= 1);
+    }
 }
